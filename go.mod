fbf7f71213e471dd2d208a77ba98417b31ea4c3d
Chris Lewis <me@chrislewis.au>
2024-12-02 08:15:31 -0500

    Day 2

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/cdlewis/advent-of-code
 
-go 1.19
+go 1.23
 
 require golang.org/x/exp v0.0.0-20221208044002-44028be4359e // indirect
---
+++ two/two.go
@@ -0,0 +1,69 @@
+package two
+
+import (
+	"iter"
+	"slices"
+	"strings"
+
+	"github.com/cdlewis/advent-of-code/util"
+)
+
+var testData = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+func Two() int {
+	reports := strings.Split(util.GetInput(2, false, testData), "\n")
+
+	safe := 0
+	for _, r := range reports {
+		report := util.Map(strings.Split(r, " "), util.ToInt[string])
+
+		valid := checkReport(report)
+		if valid {
+			safe++
+			continue
+		}
+
+		slices.Reverse(report)
+		reverseValid := checkReport(report)
+		if reverseValid {
+			safe++
+		}
+	}
+
+	return safe
+}
+
+func checkReport(reports []int) bool {
+CHECK_REPORTS:
+	for r := range reportPermutations(reports) {
+		for i, j := 0, 1; j < len(r); i, j = j, j+1 {
+			if r[i] == r[j] || r[j]-r[i] < 0 || r[j]-r[i] > 3 {
+				continue CHECK_REPORTS
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func reportPermutations(report []int) iter.Seq[[]int] {
+	return func(yield func([]int) bool) {
+		for i := -1; i < len(report); i++ {
+			newReport := report
+			if i > -1 {
+				newReport = slices.Concat(newReport[:i], newReport[i+1:])
+			}
+
+			if !yield(newReport) {
+				return
+			}
+		}
+	}
+}
